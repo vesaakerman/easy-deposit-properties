fc97e758c8a5021fef7a00bd1c8c7de18bfb4ec9
Richard van Heest <richard.van.heest@dans.knaw.nl>
2019-05-03 12:28:46 +0200

    Test resolver calls for all available examples

--- src/test/scala/nl.knaw.dans.easy.properties/server/GraphQLResolveSpec.scala
+++ src/test/scala/nl.knaw.dans.easy.properties/server/GraphQLResolveSpec.scala
@@ -17,9 +17,43 @@ import org.scalamock.scalatest.MockFactory
 import org.scalatra.test.EmbeddedJettyContainer
 import org.scalatra.test.scalatest.ScalatraSuite
 
+trait GraphQLResolveSpecTestObjects {
+  val depositId1: UUID = UUID.fromString("00000000-0000-0000-0000-000000000001")
+  val depositId2: UUID = UUID.fromString("00000000-0000-0000-0000-000000000002")
+  val depositId3: UUID = UUID.fromString("00000000-0000-0000-0000-000000000003")
+  val deposit1 = Deposit(
+    id = depositId1,
+    creationTimestamp = DateTime.now(),
+    depositorId = "user001",
+  )
+  val deposit2 = Deposit(
+    id = depositId2,
+    creationTimestamp = DateTime.now(),
+    depositorId = "user002",
+  )
+  val deposit3 = Deposit(
+    id = depositId3,
+    creationTimestamp = DateTime.now(),
+    depositorId = "user002",
+  )
+  val state1 = State(
+    label = StateLabel.SUBMITTED,
+    description = "your deposit is submitted"
+  )
+  val state2 = State(
+    label = StateLabel.DRAFT,
+    description = "your deposit is in draft"
+  )
+  val state3 = State(
+    label = StateLabel.SUBMITTED,
+    description = "your deposit is submitted"
+  )
+}
+
 class GraphQLResolveSpec extends TestSupportFixture
   with MockFactory
   with FileSystemSupport
+  with GraphQLResolveSpecTestObjects
   with EmbeddedJettyContainer
   with ScalatraSuite {
 
@@ -40,25 +74,74 @@ class GraphQLResolveSpec extends TestSupportFixture
   "graphql" should "resolve 'deposit.graphql' with two calls to the repository" in {
     val input = graphqlExamplesDir / "deposit.graphql"
 
-    val depositId = UUID.fromString("00000000-0000-0000-0000-000000000001")
-    val deposit = Deposit(
-      id = depositId,
-      creationTimestamp = DateTime.now(),
-      depositorId = "user001",
-    )
-    val state = State(
-      label = StateLabel.SUBMITTED,
-      description = "your deposit is submitted"
-    )
-
-    repository.getDeposit _ expects depositId returning Some(deposit)
-    repository.getState _ expects depositId returning Some(state)
+    inSequence {
+      repository.getDeposit _ expects depositId1 returning Some(deposit1)
+      repository.getState _ expects depositId1 returning Some(state1)
+    }
 
     runQuery(input.contentAsString)
   }
-  
-  // TODO continue testing more of our example files and test how many calls to the repository are required.
-  //  With that we can hopefully see if it is already necessary to implement batching.
+
+  it should "resolve 'listAllDeposits.graphql' with n + 1 calls to the repository" in {
+    val input = graphqlExamplesDir / "listAllDeposits.graphql"
+
+    inSequence {
+      repository.getAllDeposits _ expects() returning Seq(deposit1, deposit2, deposit3)
+      repository.getState _ expects depositId1 returning Some(state1)
+      repository.getState _ expects depositId2 returning Some(state2)
+      repository.getState _ expects depositId3 returning Some(state3)
+    }
+
+    runQuery(input.contentAsString)
+  }
+
+  it should "resolve 'listDepositsFromDepositor.graphql' with n + 1 calls to the repository" in {
+    val input = graphqlExamplesDir / "listDepositsFromDepositor.graphql"
+
+    inSequence {
+      repository.getDepositByUserId _ expects "user002" returning Seq(deposit2, deposit3)
+      repository.getState _ expects depositId2 returning Some(state2)
+      repository.getState _ expects depositId3 returning Some(state3)
+    }
+
+    runQuery(input.contentAsString)
+  }
+
+  it should "resolve 'listDepositsWithSameDepositor.graphql' with n + 2 calls to the repository" in {
+    val input = graphqlExamplesDir / "listDepositsWithSameDepositor.graphql"
+    input.writeText(input.contentAsString.replace(depositId1.toString, depositId2.toString))
+
+    inSequence {
+      repository.getDeposit _ expects depositId2 returning Some(deposit2)
+      repository.getDepositByUserId _ expects "user002" returning Seq(deposit2, deposit3)
+      repository.getState _ expects depositId2 returning Some(state2)
+      repository.getState _ expects depositId3 returning Some(state3)
+    }
+
+    runQuery(input.contentAsString)
+  }
+
+  it should "resolve 'listDepositsWithSameState.graphql' with n + 2 calls to the repository" in {
+    val input = graphqlExamplesDir / "listDepositsWithSameState.graphql"
+
+    inSequence {
+      repository.getDeposit _ expects depositId1 returning Some(deposit1)
+      repository.getState _ expects depositId1 returning Some(state1)
+      repository.getDepositByState _ expects StateLabel.SUBMITTED returning Seq(deposit1, deposit3)
+    }
+
+    runQuery(input.contentAsString)
+  }
+
+  it should "resolve 'listDepositsWithState.graphql' with n + 2 calls to the repository" in {
+    val input = graphqlExamplesDir / "listDepositsWithState.graphql"
+
+    inSequence {
+      repository.getDepositByState _ expects StateLabel.SUBMITTED returning Seq(deposit1, deposit3)
+    }
+
+    runQuery(input.contentAsString)
+  }
 
   private def runQuery(query: String) = {
     val inputBody = compact(render("query" -> query))
