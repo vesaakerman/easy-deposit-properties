29943eb2ed6527115d7eed43a6fd1e9245d1af9b
Richard van Heest <richard.van.heest@dans.knaw.nl>
2019-06-25 10:58:32 +0200

    update database schema after discussion

--- src/main/assembly/dist/install/db-tables.sql
+++ src/main/assembly/dist/install/db-tables.sql
@@ -6,51 +6,12 @@ CREATE TABLE Deposit (
 );
 
 CREATE TABLE State (
-    value VARCHAR(64) NOT NULL PRIMARY KEY
-);
-
-INSERT INTO State (value)
-VALUES ('DRAFT'),
-       ('UPLOADED'),
-       ('FINALIZING'),
-       ('INVALID'),
-       ('SUBMITTED'),
-       ('REJECTED'),
-       ('FAILED'),
-       ('IN_REVIEW'),
-       ('ARCHIVED'),
-       ('FEDORA_ARCHIVED');
-
-CREATE TABLE DepositState (
     stateId SERIAL NOT NULL PRIMARY KEY,
     depositId CHAR(36) NOT NULL,
-    state VARCHAR(64) NOT NULL,
+    label VARCHAR(64) NOT NULL,
     description TEXT NOT NULL,
     timestamp TIME WITH TIME ZONE NOT NULL,
-    FOREIGN KEY (depositId) REFERENCES Deposit (depositId),
-    FOREIGN KEY (state) REFERENCES State (value)
-);
-
-CREATE TABLE IngestStep (
-    value VARCHAR(64) NOT NULL PRIMARY KEY
-);
-
-INSERT INTO IngestStep (value)
-VALUES ('VALIDATE'),
-       ('PID_GENERATOR'),
-       ('FEDORA'),
-       ('SPRINGFIELD'),
-       ('BAGSTORE'),
-       ('BAGINDEX'),
-       ('SOLR4FILES');
-
-CREATE TABLE DepositIngestStep (
-    stepId SERIAL NOT NULL PRIMARY KEY,
-    depositId CHAR(36) NOT NULL,
-    step VARCHAR(64) NOT NULL,
-    timestamp TIME WITH TIME ZONE NOT NULL,
-    FOREIGN KEY (depositId) REFERENCES Deposit (depositId),
-    FOREIGN KEY (step) REFERENCES IngestStep (value)
+    FOREIGN KEY (depositId) REFERENCES Deposit (depositId)
 );
 
 CREATE TABLE Identifier (
@@ -70,63 +31,21 @@ CREATE TABLE DepositIdentifier (
     identifierValue VARCHAR(64) NOT NULL,
     timestamp TIME WITH TIME ZONE NOT NULL,
     FOREIGN KEY (depositId) REFERENCES Deposit (depositId),
-    FOREIGN KEY (identifier) REFERENCES Identifier (value)
---    TODO add uniqueness constraint on (depositId, identifier)
+    FOREIGN KEY (identifier) REFERENCES Identifier (value),
+    UNIQUE (depositId, identifier)
 );
 
-CREATE TABLE DoiEventType (
-    value VARCHAR(64) NOT NULL PRIMARY KEY
-);
-
-INSERT INTO DoiEventType (value)
-VALUES ('registered'),
-       ('action');
-
-CREATE TABLE DepositDoiEvent (
-    doiEventId SERIAL NOT NULL PRIMARY KEY,
+CREATE TABLE Curation (
+    curationId SERIAL NOT NULL PRIMARY KEY,
     depositId CHAR(36) NOT NULL,
-    doiEvent VARCHAR(64) NOT NULL,
-    value VARCHAR(64) NOT NULL,
-    timestamp TIME WITH TIME ZONE NOT NULL,
-    FOREIGN KEY (depositId) REFERENCES Deposit (depositId),
-    FOREIGN KEY (doiEvent) REFERENCES DoiEventType (value)
-);
-
-CREATE TABLE DepositCurator (
-    curatorId SERIAL NOT NULL PRIMARY KEY,
-    depositId CHAR(36) NOT NULL,
+    isNewVersion BOOLEAN NOT NULL,
+    isRequired BOOLEAN NOT NULL,
+    isPerformed BOOLEAN NOT NULL,
     datamanagerUserId VARCHAR(64) NOT NULL,
     datamanagerEmail TEXT NOT NULL,
     timestamp TIME WITH TIME ZONE NOT NULL,
     FOREIGN KEY (depositId) REFERENCES Deposit (depositId)
 );
-
-CREATE TABLE CurationType (
-    value VARCHAR(64) NOT NULL PRIMARY KEY
-);
-
-INSERT INTO CurationType (value)
-VALUES ('is-new-version'),
-       ('curation-required'),
-       ('curation-performed');
-
-CREATE TABLE DepositCurationEvent (
-    curationEventId SERIAL NOT NULL PRIMARY KEY,
-    depositId CHAR(36) NOT NULL,
-    curationType VARCHAR(64) NOT NULL,
-    value VARCHAR(64) NOT NULL,
-    timestamp TIME WITH TIME ZONE NOT NULL,
-    FOREIGN KEY (depositId) REFERENCES Deposit (depositId),
-    FOREIGN KEY (curationType) REFERENCES CurationType (value)
-);
-
-CREATE TABLE SpringfieldPlayMode (
-    value VARCHAR(32) NOT NULL PRIMARY KEY
-);
-
-INSERT INTO SpringfieldPlayMode (value)
-VALUES ('continuous'),
-       ('menu');
 
 CREATE TABLE DepositSpringfield (
     springfieldId SERIAL NOT NULL PRIMARY KEY,
@@ -136,29 +55,22 @@ CREATE TABLE DepositSpringfield (
     collection VARCHAR(32) NOT NULL,
     playmode VARCHAR(32) NOT NULL,
     timestamp TIME WITH TIME ZONE NOT NULL,
-    FOREIGN KEY (depositId) REFERENCES Deposit (depositId),
-    FOREIGN KEY (playmode) REFERENCES SpringfieldPlayMode (value)
+    FOREIGN KEY (depositId) REFERENCES Deposit (depositId)
 );
 
-CREATE TABLE DepositClientMessageContentType (
-    clientMessageContentTypeId SERIAL NOT NULL PRIMARY KEY,
+CREATE TABLE SimpleProperties (
+    propertyId SERIAL NOT NULL PRIMARY KEY,
     depositId CHAR(36) NOT NULL,
-    contentType VARCHAR(64) NOT NULL,
+    key VARCHAR(64) NOT NULL,
+    value VARCHAR(64) NOT NULL,
     timestamp TIME WITH TIME ZONE NOT NULL,
     FOREIGN KEY (depositId) REFERENCES Deposit (depositId)
 );
 
 GRANT INSERT, SELECT ON Deposit TO easy_deposit_properties;
 GRANT INSERT, SELECT ON State TO easy_deposit_properties;
-GRANT INSERT, SELECT ON DepositState TO easy_deposit_properties;
-GRANT INSERT, SELECT ON IngestStep TO easy_deposit_properties;
-GRANT INSERT, SELECT ON DepositIngestStep TO easy_deposit_properties;
-GRANT INSERT, SELECT ON Identifier TO easy_deposit_properties;
+GRANT SELECT ON Identifier TO easy_deposit_properties;
 GRANT INSERT, SELECT ON DepositIdentifier TO easy_deposit_properties;
-GRANT INSERT, SELECT ON DoiEventType TO easy_deposit_properties;
-GRANT INSERT, SELECT ON DepositDoiEvent TO easy_deposit_properties;
-GRANT INSERT, SELECT ON DepositCurator TO easy_deposit_properties;
-GRANT INSERT, SELECT ON CurationType TO easy_deposit_properties;
-GRANT INSERT, SELECT ON DepositCurationEvent TO easy_deposit_properties;
+GRANT INSERT, SELECT ON Curation TO easy_deposit_properties;
 GRANT INSERT, SELECT ON DepositSpringfield TO easy_deposit_properties;
-GRANT INSERT, SELECT ON DepositClientMessageContentType TO easy_deposit_properties;
+GRANT INSERT, SELECT ON SimpleProperties TO easy_deposit_properties;
--- src/test/resources/database/database.sql
+++ src/test/resources/database/database.sql
@@ -1,5 +1,5 @@
 -- differences with src/main/assembly/dist/install/db-tables.sql in order to support HSQLDB syntax
--- * SMALLSERIAL --> INTEGER IDENTITY
+-- * SERIAL --> INTEGER IDENTITY
 -- * TEXT --> CLOB
 
 CREATE TABLE Deposit (
@@ -9,52 +9,13 @@ CREATE TABLE Deposit (
     depositorId VARCHAR(64) NOT NULL
 );
 
-CREATE TABLE State (
-    value VARCHAR(64) NOT NULL PRIMARY KEY
-);
-
-INSERT INTO State (value)
-VALUES ('DRAFT'),
-       ('UPLOADED'),
-       ('FINALIZING'),
-       ('INVALID'),
-       ('SUBMITTED'),
-       ('REJECTED'),
-       ('FAILED'),
-       ('IN_REVIEW'),
-       ('ARCHIVED'),
-       ('FEDORA_ARCHIVED');
-
 CREATE TABLE DepositState (
     stateId INTEGER IDENTITY NOT NULL PRIMARY KEY,
     depositId CHAR(36) NOT NULL,
-    state VARCHAR(64) NOT NULL,
+    label VARCHAR(64) NOT NULL,
     description CLOB NOT NULL,
     timestamp TIME WITH TIME ZONE NOT NULL,
-    FOREIGN KEY (depositId) REFERENCES Deposit (depositId),
-    FOREIGN KEY (state) REFERENCES State (value)
-);
-
-CREATE TABLE IngestStep (
-    value VARCHAR(64) NOT NULL PRIMARY KEY
-);
-
-INSERT INTO IngestStep (value)
-VALUES ('VALIDATE'),
-       ('PID_GENERATOR'),
-       ('FEDORA'),
-       ('SPRINGFIELD'),
-       ('BAGSTORE'),
-       ('BAGINDEX'),
-       ('SOLR4FILES');
-
-CREATE TABLE DepositIngestStep (
-    stepId INTEGER IDENTITY NOT NULL PRIMARY KEY,
-    depositId CHAR(36) NOT NULL,
-    step VARCHAR(64) NOT NULL,
-    timestamp TIME WITH TIME ZONE NOT NULL,
-    FOREIGN KEY (depositId) REFERENCES Deposit (depositId),
-    FOREIGN KEY (step) REFERENCES IngestStep (value)
+    FOREIGN KEY (depositId) REFERENCES Deposit (depositId)
 );
 
 CREATE TABLE Identifier (
@@ -74,63 +35,21 @@ CREATE TABLE DepositIdentifier (
     identifierValue VARCHAR(64) NOT NULL,
     timestamp TIME WITH TIME ZONE NOT NULL,
     FOREIGN KEY (depositId) REFERENCES Deposit (depositId),
-    FOREIGN KEY (identifier) REFERENCES Identifier (value)
---    TODO add uniqueness constraint on (depositId, identifier)
+    FOREIGN KEY (identifier) REFERENCES Identifier (value),
+    UNIQUE (depositId, identifier)
 );
 
-CREATE TABLE DoiEventType (
-    value VARCHAR(64) NOT NULL PRIMARY KEY
-);
-
-INSERT INTO DoiEventType (value)
-VALUES ('registered'),
-       ('action');
-
-CREATE TABLE DepositDoiEvent (
-    doiEventId INTEGER IDENTITY NOT NULL PRIMARY KEY,
+CREATE TABLE Curation (
+    curationId INTEGER IDENTITY NOT NULL PRIMARY KEY,
     depositId CHAR(36) NOT NULL,
-    doiEvent VARCHAR(64) NOT NULL,
-    value VARCHAR(64) NOT NULL,
-    timestamp TIME WITH TIME ZONE NOT NULL,
-    FOREIGN KEY (depositId) REFERENCES Deposit (depositId),
-    FOREIGN KEY (doiEvent) REFERENCES DoiEventType (value)
-);
-
-CREATE TABLE DepositCurator (
-    curatorId INTEGER IDENTITY NOT NULL PRIMARY KEY,
-    depositId CHAR(36) NOT NULL,
+    isNewVersion BOOLEAN NOT NULL,
+    isRequired BOOLEAN NOT NULL,
+    isPerformed BOOLEAN NOT NULL,
     datamanagerUserId VARCHAR(64) NOT NULL,
     datamanagerEmail CLOB NOT NULL,
     timestamp TIME WITH TIME ZONE NOT NULL,
     FOREIGN KEY (depositId) REFERENCES Deposit (depositId)
 );
-
-CREATE TABLE CurationType (
-    value VARCHAR(64) NOT NULL PRIMARY KEY
-);
-
-INSERT INTO CurationType (value)
-VALUES ('is-new-version'),
-       ('curation-required'),
-       ('curation-performed');
-
-CREATE TABLE DepositCurationEvent (
-    curationEventId INTEGER IDENTITY NOT NULL PRIMARY KEY,
-    depositId CHAR(36) NOT NULL,
-    curationType VARCHAR(64) NOT NULL,
-    value VARCHAR(64) NOT NULL,
-    timestamp TIME WITH TIME ZONE NOT NULL,
-    FOREIGN KEY (depositId) REFERENCES Deposit (depositId),
-    FOREIGN KEY (curationType) REFERENCES CurationType (value)
-);
-
-CREATE TABLE SpringfieldPlayMode (
-    value VARCHAR(32) NOT NULL PRIMARY KEY
-);
-
-INSERT INTO SpringfieldPlayMode (value)
-VALUES ('continuous'),
-       ('menu');
 
 CREATE TABLE DepositSpringfield (
     springfieldId INTEGER IDENTITY NOT NULL PRIMARY KEY,
@@ -140,14 +59,14 @@ CREATE TABLE DepositSpringfield (
     collection VARCHAR(32) NOT NULL,
     playmode VARCHAR(32) NOT NULL,
     timestamp TIME WITH TIME ZONE NOT NULL,
-    FOREIGN KEY (depositId) REFERENCES Deposit (depositId),
-    FOREIGN KEY (playmode) REFERENCES SpringfieldPlayMode (value)
+    FOREIGN KEY (depositId) REFERENCES Deposit (depositId)
 );
 
-CREATE TABLE DepositClientMessageContentType (
-    clientMessageContentTypeId INTEGER IDENTITY NOT NULL PRIMARY KEY,
+CREATE TABLE SimpleProperties (
+    propertyId INTEGER IDENTITY NOT NULL PRIMARY KEY,
     depositId CHAR(36) NOT NULL,
-    contentType VARCHAR(64) NOT NULL,
+    key VARCHAR(64) NOT NULL,
+    value VARCHAR(64) NOT NULL,
     timestamp TIME WITH TIME ZONE NOT NULL,
     FOREIGN KEY (depositId) REFERENCES Deposit (depositId)
 );
